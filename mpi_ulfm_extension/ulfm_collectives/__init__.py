899b4d59a8d82ffde7d44ec03d5b8a0cf533592d
Alvin Liu <ziyueliu@ucsb.edu>
2026-04-15 17:08:03 -0700

    Export HSDP integration symbols from ulfm_collectives

--- mpi_ulfm_extension/ulfm_collectives/__init__.py
+++ mpi_ulfm_extension/ulfm_collectives/__init__.py
@@ -6,3 +6,14 @@ from ulfm_collectives._C import (  # noqa: F401
     set_ulfm_verbose_logging,
     is_ulfm_verbose_logging,
 )
+
+# HSDP integration
+from .hsdp_groups import (
+    HSDPLayout,
+    compute_hsdp_layout,
+    replica_ranks,
+    replicate_peer_ranks,
+    replica0_ranks,
+)
+from .hsdp_training_manager import HSDPULFMTrainingManager
+from .ulfm_hook import HSDPHookState, create_ulfm_hsdp_hook
